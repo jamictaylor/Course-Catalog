eca023845046ef06aff0c3a71eb6ce7921c797c3
jamitaylor <jamitaylor@mail.weber.edu>
2022-05-30 14:12:34 -0600

    Initial Commit

---
+++ Course Catalog/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Course Catalog
+//
+//  Created by Jami Taylor on 5/30/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Course Catalog/Course_CatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  Course_CatalogApp.swift
+//  Course Catalog
+//
+//  Created by Jami Taylor on 5/30/22.
+//
+
+import SwiftUI
+
+@main
+struct Course_CatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
